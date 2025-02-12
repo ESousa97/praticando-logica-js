951565b8b576c1618963d728d73425e6b3edba1d
Sousa <114963739+ESousa97@users.noreply.github.com>
2025-02-11 21:14:21 -0300

    Realizada a soma de duas variaveis numericas.

---
+++ variaveis-numericas-dia-11/js/app.js
@@ -0,0 +1,9 @@
+// Faça a soma de duas variáveis numéricas e apresente no console com uma mensagem amigável em Template String.
+let numero = 10;
+let numero2 = 20;
+
+function soma(numero, numero2) {
+  return numero + numero2;
+}
+
+console.log(`A soma de ${numero} e ${numero2} é igual a ${soma(numero, numero2)}`);
